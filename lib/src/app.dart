7a6eb7c99631790063da357536ce67baaf9c1d3b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-15 16:46:23 +0100

    little changes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:hive_app_example/screens/home.dart';
+// import 'package:hive_app_example/screens/home.dart';
+import 'package:hive_app_example/screens/todo.dart';
 
 class App extends StatelessWidget {
   @override
@@ -9,11 +10,11 @@ class App extends StatelessWidget {
     return defaultTargetPlatform == TargetPlatform.android
         ? MaterialApp(
           title: "Hive Basic Tut",
-          home: Home(),
+          home: TodoScreen(),
         )
         : CupertinoApp(
           title: "Hive Basic Tut",
-          home: Home(),
+          home: TodoScreen(),
         );
   }
 }
