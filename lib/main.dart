67f8ed98a5566a0c45381584f15207430dab3405
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-06-15 16:46:01 +0100

    config box for todo storage

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_app_example/const_value.dart';
+import 'package:hive_app_example/models/todo_model.dart';
 import 'package:hive_app_example/src/app.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   Directory document = await getApplicationDocumentsDirectory();
+  Hive.registerAdapter(TodoModelAdapter());// to register the adapter
   Hive.init(document.path);
-  await Hive.openBox<String>("friends"); // like a database table
+  await Hive.openBox<TodoModel>(todoBox); // like a database table
   runApp(App());
 }
